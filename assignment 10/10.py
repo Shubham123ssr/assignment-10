a97022b59370e0ae48e3c7f8c65b0af98482bb09
Shubham123ssr <100044183+Shubham123ssr@users.noreply.github.com>
2022-09-14 22:04:45 +0530

    Add files via upload

---
+++ assignment 10/10.py
@@ -0,0 +1,66 @@
+#q1
+'''
+n=int(input("Enter the value of n "))
+for e in range(1,n):
+    print("MySirG")
+'''
+#q2
+'''
+n=int(input("Enter the value of n "))
+for e in range(1,n+1):
+    print(e)
+'''
+#q3
+'''
+n=int(input("Enter the value of n "))
+for e in range(n,0,-1):
+    print(e)
+'''
+#q4
+'''
+n=int(input("Enter the value of n "))
+for e in range(1,2*n,2):
+    print(e)
+'''
+#q5
+'''
+n=int(input("Enter the value of n "))
+for e in range(2*n-1,0,-2):
+    print(e)
+    '''
+#q6
+'''
+n=int(input("Enter the value of n "))
+for e in range(2,2*n+2,2):
+    print(e)
+'''
+#q7
+'''
+n=int(input("Enter the value of n "))
+for e in range(2*n,0,-2):
+    print(e)
+'''
+#q8
+'''
+n=int(input("Enter the value of n "))
+for e in range(1,n+1,1):
+    print(e*e)
+'''
+#q9
+'''
+n=int(input("Enter the value of n "))
+for e in range(1,n+1,1):
+    print(e*e*e)
+    '''
+#q10
+'''
+n=int(input("Enter the value of n "))
+for e in range(1,11):
+    print(10*e)
+    '''
+
+
+
+
+
+
